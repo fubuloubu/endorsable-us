87f49495cc83f544b3b9d0a22ae9f5acdbfad001
Bryant Eisenbach <fubuloubu@gmail.com>
2017-06-25 23:50:51 -0700

    Started adding some application logic... not gonig well

---
+++ src/firebase-user-logic.js
@@ -0,0 +1,19 @@
+// If user is not in system, add them!
+function add_user() {
+    var user_ref = window.database.ref('users/' + window.user.uid);
+    if (!user_ref) {
+        user_ref.set({
+            name: window.user.displayName,
+            photo: window.user.photoURL,
+        });
+    }
+}
+
+//Invite a new user (anonmyous?)
+//Add endorsement to another user
+//Accept and endorsement
+//Amend an endorsement
+//Reject an endorsement
+
+//Get endorsements (with the given filter)
+//Get skills (with the given filter)
