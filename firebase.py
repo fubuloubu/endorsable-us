8b32131ecb80dab7e7478793d39e8e3e37a30634
Bryant <fubuloubu@gmail.com>
2017-06-30 19:34:12 -0700

    Redid some of the error texts printing to route to the page. Also added all the CRUD methods

--- firebase.py
+++ firebase.py
@@ -22,10 +22,27 @@ class User(object):
         self._id_token = user['idToken']
         self._refresh_token = user['refreshToken']
         self._timeout = user['expiresIn']
+        
+    def _get_db_data(self, key):
+        return self._db.child(key).get(self._id_token).val()
 
     def _set_db_data(self, key, data):
         self._db.child(key).set(data, self._id_token)
 
+    def _upd_db_data(self, key, data):
+        self._db.child(key).update(data, self._id_token)
+        
+    def _del_db_data(self, key):
+        return self._db.child(key).remove(self._id_token)
+
+    def _push_db_data(self, key, data):
+        self._db.child(key).push(data, self._id_token)
+
+    def _pop_db_data(self, key):
+        data = self._get_db_data(key)
+        self._del_db_data(key)
+        return data
+        
     def get_uid(self):
         if self._uid is not None:
             return self._uid
@@ -36,9 +53,11 @@ class User(object):
         try:
             user = self._auth.sign_in_with_email_and_password(email, password)
             self._set_auth_data(user)
-            return self.authenticated()
-        except request_exceptions.HTTPError as e:
-            return False # Login unsuccessful
+            return None if self.authenticated() else 'Not sure what happened here...'
+        except request_exceptions.HTTPError as error:
+            #'Your password is incorrect or you are not registered in the system' + \
+            #        '<br><br>You can register ' + create_link('register', 'here')
+            return error # Login unsuccessful
 
     def register(self, fullname, email, password):
         try:
@@ -47,9 +66,11 @@ class User(object):
             # Add user data to our db
             userdata = {"email": email, "name": fullname}
             self._set_db_data('users', {self.get_uid() : userdata})
-            return self.authenticated()
-        except request_exceptions.HTTPError as e:
-            return False # Registration unsuccessful
+            return None if self.authenticated() else 'Not sure what happened here...'
+        except request_exceptions.HTTPError as error:
+            #'The email you provided is already in use' + \
+            #        '<br><br>You can login ' + create_link('login', 'here')
+            return error # Registration unsuccessful
 
     def _refresh_session(self):
         user = self._auth.refresh(self.refresh_token)
@@ -63,3 +84,44 @@ class User(object):
     # User has authenticated and has an active session token
     def authenticated(self):
         return self._id_token is not None and not self._is_session_expired()
+
+    def get_user_data(self, user_uid):
+        return self._get_db_data('users/' + user_uid)
+
+    def get_user_relationships(self):
+        return self._get_db_data('relationships/' + self.get_uid())
+
+    def get_endorsements_by_uid(self, user_uid):
+        return self._get_db_data('endorsements/' + user_uid)
+
+    def get_user_endorsements(self):
+        return self.get_endorsements_by_uid(self.get_uid())
+
+    def get_pending_endorsements(self):
+        return self._get_db_data('pending/' + self.get_uid())
+
+    def add_endorsement(self, uid_to, endorsement_text, time):
+        endorsement = {
+            "text" : endorsement_text,
+            "from" : self.get_uid(),
+            "time" : time,
+            "to"   : uid_to, # This is used to keep track of the
+                             # final approver, aka which user can
+                             # publish the endorsement
+        }
+        self._push_db_data('pending/' + uid_to, endorsement)
+
+    def accept_pending(self, pending_uid):
+        endorsement = self._pop_db_data('pending/' + self.get_uid() + '/' + endorsement_uid)
+        uid_to = endorsement['to']
+        del endorsement['to']
+        self._push_db_data('endorsements/' + uid_to, endorsement)
+
+    def amend_pending(self, pending_uid, updated_text):
+        endorsement = self._pop_db_data('pending/' + self.get_uid() + '/' + endorsement_uid)
+        uid_to = endorsement['to']
+        endorsement['text'] = updated_text
+        self._push_db_data('pending/' + uid_to, endorsement)
+
+    def reject_pending(self, pending_uid):
+        self._pop_db_data('pending/' + self.get_uid() + '/' + endorsement_uid)
