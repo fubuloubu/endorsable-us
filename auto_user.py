9e3aba6ca1412da5df12b9ba5cc9432820f3748c
Bryant <fubuloubu@gmail.com>
2017-07-01 23:26:32 -0700

    Needed to play around with self.uid and the polling method

--- auto_user.py
+++ auto_user.py
@@ -1,5 +1,5 @@
 from basic_user import BasicUser
-import polling
+from time import sleep
 
 class AutoUser(BasicUser):
     def __init__(self, name='John Smith', email='johnsmith@johnsonsjohnsons.net'):
@@ -13,14 +13,12 @@ class AutoUser(BasicUser):
             'email' : email,
             'password' : 'password'
         }
-        error = self.register(registration_data)
+        error = self.login(self.login_data)
         if error:
-            self.login(self.login_data)
-        self.uid = super().get_uid() # Store it so we don't have to constantly reference it
+            self.register(registration_data)
+        self.uid = self.get_uid() # Store it so we don't have to constantly reference it
+        # Some reason this fails when registering
         print('({0}) Logged in'.format(self.get_uid()))
-
-    def get_uid(self):
-        return self.uid
 
     def __deinit__(self):
         self.logout()
@@ -31,7 +29,7 @@ class AutoUser(BasicUser):
             'friend_uid' : friend['uid'],
             'relationship' : rtype
         }
-        print('({0}) Add {2} {1}'.format(self.get_uid(), friend['uid'], rtype))
+        print('({0}) Add {2} {1}'.format(self.uid, friend['uid'], rtype))
         super().add_relationship(formdata)
 
     def add_endorsement(self, to, endorsement_text='Robots have nothing interesting to say'):
@@ -40,14 +38,14 @@ class AutoUser(BasicUser):
             'endorsement_text' : endorsement_text,
             'time' : 'now'
         }
-        print('({0}) Add endr (to={1})'.format(self.get_uid(), to['uid']))
+        print('({0}) Add endr (to={1})'.format(self.uid, to['uid']))
         super().add_endorsement(formdata)
 
     def accept_pending(self, pending):
         formdata = {
             'pending_uid' : pending['key'],
         }
-        print('({0}) Accept endr (id={1})'.format(self.get_uid(), pending['key']))
+        print('({0}) Accept endr (id={1})'.format(self.uid, pending['key']))
         super().accept_pending(formdata)
 
     def amend_pending(self, pending_uid, updated_text='Yes we do!'):
@@ -55,14 +53,14 @@ class AutoUser(BasicUser):
             'pending_uid' : pending['key'],
             'updated_text' : updated_text
         }
-        print('({0}) Amend endr (id={1})'.format(self.get_uid(), pending['key']))
+        print('({0}) Amend endr (id={1})'.format(self.uid, pending['key']))
         super().amend_pending(formdata)
 
     def reject_pending(self, pending_uid):
         formdata = {
             'pending_uid' : pending['key'],
         }
-        print('({0}) Reject endr (id={1})'.format(self.get_uid(), pending['key']))
+        print('({0}) Reject endr (id={1})'.format(self.uid, pending['key']))
         super().reject_pending(formdata)
     
     # Automated Methods
@@ -78,27 +76,30 @@ class AutoUser(BasicUser):
         # Default is to befriend all people
         self.add_relationship(user)
 
-    def run_behavior(self):
+    def run_behavior(self, pausetime=1):
         for p in self.get_pending_endorsements():
             self.pending_behavior(p)
+            sleep(pausetime) # Added so it doesn't make too many requests at a time
         
         my_peeps = self.get_relationships()
         for u in my_peeps:
             self.relationship_behavior(u)
+            sleep(pausetime) # Added so it doesn't make too many requests at a time
         
         my_peeps = [u['uid'] for u in my_peeps]
         other_peeps = [u for u in self.get_all_users() \
-                if u['uid'] not in my_peeps and u['uid'] != self.get_uid()] 
+                if u['uid'] not in my_peeps and u['uid'] != self.uid] 
         for u in other_peeps:
             self.unrelated_behavior(u)
+            sleep(pausetime) # Added so it doesn't make too many requests at a time
 
         # Return true if there any reasons to exit polling
         return not self.authenticated()
 
-    def use_website(self, **kwargs):
+    def use_website(self, timeout, step):
         # Poll the website for updates
-        polling.poll(self.run_behavior, **kwargs)
-
-if __name__ == '__main__':
-    john = AutoUser()
-    john.use_website(timeout=180, step=30)
+        while timeout > 0:
+            self.run_behavior()
+            sleep(step)
+            timeout -= step
+        #polling.poll(self.run_behavior, **kwargs)
